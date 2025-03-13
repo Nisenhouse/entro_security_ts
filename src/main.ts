0aebe25d7fb78ccbd7d4973e496dfe7ff06e501f
Nisenhouse <dor.nisenhouse@gmail.com>
2025-03-13 21:46:47 +0000

    Getting GitHub commit data

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
 import {getInput, InputData} from './input';
 
-async function getData() {
-  const response = await fetch('https://api.github.com/users/github');
+async function main() {
+  //const inputData = await getInput();
+  const inputData = {
+    owner: "Nisenhouse",
+    repo: "entro_security",
+    token: "xxxx"
+  };
+  console.table(inputData);
+  const commits = await getCommits(inputData.owner, inputData.repo, inputData.token)
+  console.log(commits);
+}
+
+function getRequestOpts(token: string) : RequestInit {
+  const headers: HeadersInit = {
+    'Accept': 'application/vnd.github.v3+json',
+    'Authorization': `token ${token}`
+  }
+  const opts: RequestInit = {
+      headers
+  };
+  return opts;
+}
+
+async function getCommits(owner: string, repo: string, token: string) {
+  const response = await fetch(
+    `https://api.github.com/repos/${owner}/${repo}/commits`,
+    getRequestOpts(token));
+  if (response.status != 200) {
+    return null;
+  }
   const data = await response.json();
-  console.log(data);
   return data;
 }
 
@@ -13,8 +40,6 @@ async function getData() {
 
 
 
-getData().then((data) => {
-  console.table(data);
-});
+main();
 
 
